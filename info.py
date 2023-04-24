6d9f7e209bb52c266c85090323ee553eefb20dc5
Ravi44984 <114041585+Ravi44984@users.noreply.github.com>
2023-04-24 12:24:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 9868987
+API_HASH     = "635bb29b0e20b8fe700a37ccbf0138a5"
+BOT_TOKEN    = "6104695060:AAEPhX_IhHhIH2g_LB5ngYA_9iSWz1Hk7ME"
+SESSION      = "AQAjUgGNYWo-NgxBl5KydkaTpuuFTTslS89BOW38w3WRCBZ-bJ5VW6yCKVoaPVcRwnQJ145uWySN8GwXLKOlk1R_U0UugVys1-_9KXD06X0YDh5BMrYB0RNyRC5YxdQhVxj1W3XSneyJv5GeIHmQ5kQ-kYeWuklLHx6HTwwfkStxO6yXgvWvmfKFP7ucdUu5Q6x2-M0EdrytQMthxrBy_Yc8nEGCBWCr2ukc5GMFlspveYmJ-xNbbZ8odpBN70SY4t5ysxMD-f0B4B48-fBdsgjMtyVoVY7ct-Li6hxpm20OUe-3XA77G7hbc2QTNZjemXHYx5Cd9lFeUn1JSVSWee0adcKhLAA"
+DATABASE_URI = "mongodb+srv://Searchrobot:<Ravi12345>@searchbot.ii6grkg.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001848949357
+ADMIN        = 1975689516
+CHANNEL      = "@moviesearchbybot"
